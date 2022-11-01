f8d56f890e19315ab51032706a694612bba8ebf3
skyone <1119858711@qq.com>
2022-11-01 05:26:16 +0000

    Format save gas

--- src/MySolution.sol
+++ src/MySolution.sol
@@ -6,20 +6,7 @@ import {Fixed18} from "src/Fixed18.sol";
 
 contract MySolution is ISqrt {
         function sqrt(Fixed18[INPUT_SIZE] calldata input) external pure returns (Fixed18[INPUT_SIZE] memory output) {
-            // for (uint256 i = 0; i < input.length; ++i) {
-            //     unchecked {
-            //         Fixed18 z = input[i].div(Fixed18.wrap(2 * 10 ** 18));
-            //         Fixed18 y = input[i];
-            //         while (y.gt(z)) {
-            //             y = z;
-            //             z = input[i].div( z.mul(Fixed18.wrap(10**36))).add(z).div(Fixed18.wrap(2 * 10 ** 18));
-            //         }
-            //         output[i] = z.mul(Fixed18.wrap(10**27));
-            //     }
-            // }
-
         for (uint256 i = 0; i < input.length; ++i) {
-            // emit Loginfo("Original Value", a);
             unchecked{
                 Fixed18 tmp_a = input[i];
                 Fixed18 y = input[i];
